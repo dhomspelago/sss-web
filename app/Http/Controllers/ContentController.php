68ade0e9d1cfb82a63bd4211f9b687187ad1b999
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-04-21 22:59:53 +0800

    add contents table model and controller

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Content;
+
+class ContentController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show(Content $content)
+    {
+        //
+    }
+
+    public function update(Content $content)
+    {
+        //
+    }
+
+    public function destroy(Content $content)
+    {
+        //
+    }
+}
