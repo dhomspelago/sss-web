aff1bd98b5bde1df241bf5454d978945dbefca1d
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-04-22 22:44:41 +0800

    add dashboard layout and users

--- routes/backend.php
+++ routes/backend.php
@@ -1,11 +1,18 @@
 <?php
 
 
+use App\Http\Controllers\Backend\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/dashboard', function () {
-    return 'test backend';
-})->middleware('auth:backend');
-
 require __DIR__ . '/backend-auth.php';
+
+Route::middleware('auth:backend')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('backend.dashboard');
+    })->name('dashboard');
+
+    Route::prefix('users')->name('users')->group(function () {
+        Route::get('', [UserController::class, 'index']);
+    });
+});
